2eea8d0850319f32dc14843deb2123bb4d0d38e6
Marijn Haverbeke <marijn@haverbeke.nl>
2016-07-11 14:23:20 +0200

    Make the set of fields in the editor state extendable

--- demo/demo.js
+++ demo/demo.js
@@ -5,6 +5,6 @@ const {baseKeymap} = require("../src/commands")
 
 let state = EditorState.fromDoc(schema.parseDOM(document.querySelector("#content")))
 let view = new EditorView(document.querySelector(".full"), state, {
-  onChange(state) { view.update(state) },
+  onChange(state) { view.update(window.pmState = state) },
   keymaps: [baseKeymap]
 })
