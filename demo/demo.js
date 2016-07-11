53b0bb5dc8156ccd88a534f01073d25839fb0ae4
Marijn Haverbeke <marijn@haverbeke.nl>
2016-07-11 16:59:21 +0200

    Provide a plugin architecture through an external Configuration object
    Which allows plugins to
    
     - Add to composable props
     - Add state fields
     - Specify dependencies on each other

--- demo/demo.js
+++ demo/demo.js
@@ -2,9 +2,13 @@ const {EditorState} = require("../src/state")
 const {schema} = require("../src/schema-basic")
 const {EditorView} = require("../src/view")
 const {baseKeymap} = require("../src/commands")
+const {Configuration, createPluginIdentity} = require("../src/config")
 
-let state = EditorState.fromDoc(schema.parseDOM(document.querySelector("#content")))
-let view = new EditorView(document.querySelector(".full"), state, {
+const config = new Configuration([
+  {keymaps: [baseKeymap]}
+])
+
+let state = config.stateFromDoc(schema.parseDOM(document.querySelector("#content")))
+let view = new EditorView(document.querySelector(".full"), state, config.props({
   onChange(state) { view.update(window.pmState = state) },
-  keymaps: [baseKeymap]
-})
+}))
