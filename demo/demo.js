5d99e8a27c3d6a1eb94220326b969b5215ef3243
Marijn Haverbeke <marijn@haverbeke.nl>
2016-07-20 17:08:52 +0200

    Start updating the menu module

--- demo/demo.js
+++ demo/demo.js
@@ -4,11 +4,23 @@ const {EditorView} = require("../src/view")
 const {baseKeymap} = require("../src/commands")
 const {history} = require("../src/history")
 const {inputRules, allInputRules} = require("../src/inputrules")
+const {MenuBar, liftItem, selectParentNodeItem, undoItem, redoItem} = require("../src/menu")
 
 const config = baseConfig.extend([history(), inputRules({rules: allInputRules})])
 let state = config.createState({doc: schema.parseDOM(document.querySelector("#content"))})
-let view = new EditorView(document.querySelector(".full"), state, {
+let place = document.querySelector(".full")
+
+function onChange(state) {
+  view.update(state)
+  menuBar.update(state)
+}
+
+let menuBar = new MenuBar(place, state, {
+  content: [[liftItem, selectParentNodeItem], [undoItem, redoItem]],
+  onChange
+})
+let view = new EditorView(place, state, {
   keymaps: [baseKeymap],
-  onChange(state) { view.update(window.pmState = state) },
+  onChange,
   config
 })
