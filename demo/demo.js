7ef1b1886cbd4af1095c4003e88d156ed49effb2
Marijn Haverbeke <marijn@haverbeke.nl>
2016-07-20 14:33:45 +0200

    Start porting input rules to new architecture

--- demo/demo.js
+++ demo/demo.js
@@ -3,8 +3,9 @@ const {schema} = require("../src/schema-basic")
 const {EditorView} = require("../src/view")
 const {baseKeymap} = require("../src/commands")
 const {history} = require("../src/history")
+const {inputRules, allInputRules} = require("../src/inputrules")
 
-const config = baseConfig.extend([history()])
+const config = baseConfig.extend([history(), inputRules({rules: allInputRules})])
 let state = config.createState({doc: schema.parseDOM(document.querySelector("#content"))})
 let view = new EditorView(document.querySelector(".full"), state, {
   keymaps: [baseKeymap],
