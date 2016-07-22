83419b592a8a23f05178f337bcefa6f14d4448a8
Marijn Haverbeke <marijn@haverbeke.nl>
2016-07-22 23:28:19 +0200

    Make the demo work again

--- demo/demo.js
+++ demo/demo.js
@@ -1,31 +1,28 @@
-const {baseConfig} = require("../src/state")
+const {EditorState} = require("../src/state")
 const {schema} = require("../src/schema-basic")
 const {EditorView} = require("../src/view")
 const {baseKeymap} = require("../src/commands")
+const {keymap} = require("../src/keymap")
 const {history} = require("../src/history")
 const {inputRules, allInputRules} = require("../src/inputrules")
 const {MenuBar, liftItem, selectParentNodeItem, undoItem, redoItem} = require("../src/menu")
 
-const config = baseConfig//.extend([history(), inputRules({rules: allInputRules})])
-let state = config.createState({doc: schema.parseDOM(document.querySelector("#content"))})
+const historyPlugin = history()
+const plugins = [historyPlugin, inputRules({rules: allInputRules}), keymap(baseKeymap)]
+let state = EditorState.create({doc: schema.parseDOM(document.querySelector("#content")),
+                                plugins})
 let place = document.querySelector(".full")
 
 function onAction(action) {
   let state = view.state.applyAction(action)
   view.update(state)
-//  menuBar.update(state)
+  menuBar.update(state)
 }
 
-let view = new EditorView(place, state, {
-  onAction,
-  config,
-  onKey(state, keyName) {
-    let found = baseKeymap.lookup(keyName)
-    return found && found(state, onAction)
-  }
-})
-/*let menuBar = new MenuBar(view, state, {
-  content: [[liftItem, selectParentNodeItem], [undoItem, redoItem]],
+let view = new EditorView(place, state, {onAction, plugins})
+let menuBar = new MenuBar(view, state, {
+  content: [[liftItem, selectParentNodeItem], [undoItem(historyPlugin), redoItem(historyPlugin)]],
   float: true,
-  onChange
-})*/
+  something: 22,
+  onAction
+})
