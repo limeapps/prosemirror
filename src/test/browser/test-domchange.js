bf7bef34656e8b0e42a3b87b21c38319a1282011
Marijn Haverbeke <marijn@haverbeke.nl>
2016-07-25 12:43:55 +0200

    Rename onKey... to handleKey... for consistency

--- src/test/browser/test-domchange.js
+++ src/test/browser/test-domchange.js
@@ -80,7 +80,7 @@ test("detect_enter", view => {
   readInputChange(view, view.state)
   cmp(view.enterPressed, true)
 }, {doc: doc(blockquote(p("foo"), p("<a>"))),
-    onKeyDown: (view, event) => { if (event.keyCode == 13) return view.enterPressed = true }})
+    handleKeyDown: (view, event) => { if (event.keyCode == 13) return view.enterPressed = true }})
 
 test("composition_simple", view => {
   findTextNode(view.content, "hello").nodeValue = "hellox"
