714cb14742a0a23581d7c8b55a61dfb99c936477
Marijn Haverbeke <marijn@haverbeke.nl>
2016-08-30 16:39:14 +0200

    Remove last use of dom.elt
    Replace with crel in the menu bar

--- src/util/dom.js
+++ src/util/dom.js
@@ -1,28 +1,3 @@
-function elt(tag, attrs, ...args) {
-  let result = document.createElement(tag)
-  if (attrs) for (let name in attrs) {
-    if (name == "style")
-      result.style.cssText = attrs[name]
-    else if (attrs[name] != null)
-      result.setAttribute(name, attrs[name])
-  }
-  for (let i = 0; i < args.length; i++) add(args[i], result)
-  return result
-}
-exports.elt = elt
-
-function add(value, target) {
-  if (typeof value == "string")
-    value = document.createTextNode(value)
-
-  if (Array.isArray(value)) {
-    for (let i = 0; i < value.length; i++) add(value[i], target)
-  } else if (value) {
-    target.appendChild(value)
-  }
-}
-
-
 let accumulatedCSS = "", cssNode = null
 
 function insertCSS(css) {
